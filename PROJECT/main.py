2c2d1ce8a197eb485fc29bea6bf18d142e6199de
AlisMori <bobyleva0523@bk.ru>
2022-10-13 11:50:08 +0300

    Create DatePicker

--- PROJECT/main.py
+++ PROJECT/main.py
@@ -1,5 +1,6 @@
 from kivy.lang import Builder
 from kivymd.app import MDApp
+from kivymd.uix.pickers import MDDatePicker
 
 
 class Med(MDApp):
@@ -17,6 +18,17 @@ class Med(MDApp):
 
     def comp(self):
         self.screen.ids.compatibility.text = 'УРААА'
+
+    def on_save(self, instance, value, date_range):
+        self.screen.ids.date.text = f'{str(date_range[0])} - {str(date_range[-1])}'
+
+    def on_cancel(self, instance, value):
+        self.screen.ids.date.text = "You clicked Cancel"
+
+    def calendar(self):
+        date_dialog = MDDatePicker(mode='range')
+        date_dialog.bind(on_save=self.on_save, on_cancel=self.on_cancel)
+        date_dialog.open()
 
     def build(self):
         return self.screen
